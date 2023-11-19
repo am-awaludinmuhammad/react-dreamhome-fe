7f544540bac3ca1028ea7c6c945aec6cea32bd8b
caqwal <am.awaludinmuhammad@gmail.com>
2023-11-19 20:53:03 +0900

    init register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {
     Button,
     Form,
@@ -8,9 +8,44 @@ import {
     Col
 } from 'react-bootstrap';
 import Layout from '../pages/Layout';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+    const baseUrl = 'http://localhost:9000/api/v1/users';
+    const navigate = useNavigate()
+    const formData = {
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+    }
+
+    const [responseBody, setResponseBody] = useState(formData);
+
+    const inputChangeHandler = (event) => {
+        const { name, value } = event.target;
+        setResponseBody({ ...responseBody, [name]: value })
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        register(responseBody)
+    }
+
+    const register = async ({ name, username, email, password, password_confirmation }) => {
+
+        try {
+            const response = await axios.post(baseUrl, { name, username, email, password, password_confirmation });
+            if (response.data.data.id) {
+                navigate('/login')
+            }
+        } catch (error) {
+            console.log(error.stack)
+        }
+    }
+
     return (
         <Layout>
             <Container className="auth-container">
@@ -21,26 +56,26 @@ const Register = () => {
                             <Card.Body>
                                 <Card.Title className="text-center">Register</Card.Title>
                                 <hr />
-                                <Form>
+                                <Form onSubmit={onSubmitHandler}>
                                     <Form.Group className="mb-3" controlId="fullName">
                                         <Form.Label>Full Name</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter full name" />
+                                        <Form.Control onChange={(e) => inputChangeHandler(e)} name="name" type="text" placeholder="Enter full name" />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="username">
                                         <Form.Label>Username</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter username" />
+                                        <Form.Control onChange={(e) => inputChangeHandler(e)} name="username" type="text" placeholder="Enter username" />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="email">
                                         <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" />
+                                        <Form.Control onChange={(e) => inputChangeHandler(e)} name="email" type="email" placeholder="Enter email" />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="password">
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" placeholder="Password" />
+                                        <Form.Control onChange={(e) => inputChangeHandler(e)} name="password" type="password" placeholder="Password" />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="passwordConfirmation">
                                         <Form.Label>Password Confirmation</Form.Label>
-                                        <Form.Control type="password" placeholder="Password" />
+                                        <Form.Control onChange={(e) => inputChangeHandler(e)} name="password_confirmation" type="password" placeholder="Password" />
                                     </Form.Group>
 
                                     <Button variant="primary" type="submit" className="w-100 mt-2">
