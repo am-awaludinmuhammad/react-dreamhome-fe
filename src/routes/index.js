8b41075da772d69d488743064567ba853a06e14d
caqwal <am.awaludinmuhammad@gmail.com>
2023-11-23 06:37:42 +0900

    add error page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,16 @@ import PathConstants from "./pathConstants"
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import ProtectedRoute from "../lib/ProtectedRoute";
+import ErrorPage from "../pages/ErrorPage";
 
 const routes = [
+    {
+        path: '*',
+        element: <ErrorPage />,
+        title: 'Page Not Found'
+    },
     {
         path: PathConstants.HOME,
         element: <Home />,
@@ -19,6 +27,11 @@ const routes = [
         path: PathConstants.LOGIN,
         element: <Login />,
         title: "Login"
+    },
+    {
+        path: PathConstants.PROFILE,
+        element: <ProtectedRoute> <Profile /> </ProtectedRoute>,
+        title: "Profile"
     }
 ]
 
