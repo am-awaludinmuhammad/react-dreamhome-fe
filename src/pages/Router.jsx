c0b9a156a7b60a33e5ea10c47245e02ce3f77dc1
caqwal <am.awaludinmuhammad@gmail.com>
2023-11-23 07:09:11 +0900

    navigate to home if loged in

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,8 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import routes from "../routes";
+import PathConstants from "../routes/pathConstants";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Router = () => {
+  const { user } = useAuthContext();
+
   const pageRoutes = routes.map(({ path, title, element }) => {
+    if (user && [PathConstants.LOGIN, PathConstants.REGISTER].includes(path)) {
+      element = <Navigate to={PathConstants.HOME} />
+    }
+
     return <Route key={title} path={`/${path}`} element={element} />;
   });
 
