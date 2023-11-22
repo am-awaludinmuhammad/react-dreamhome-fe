b357fa5667d0f5b8e5f50847e820c36a4d1b2983
caqwal <am.awaludinmuhammad@gmail.com>
2023-11-22 20:27:50 +0900

    init login

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { useAuthContext } from "./useAuthContext"
+import { axiosInstance } from "../lib/axios";
+import { getInputErrorMessages } from "../lib/helpers";
+
+export const useLogin = () => {
+    const [errors, setErrors] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const {dispatch} = useAuthContext(null);
+
+    const login = async ({ username, password }) => {
+        setIsLoading(true);
+        setErrors(null);
+
+        try {
+            const { data:response } = await axiosInstance.post('/auth/login', { username, password });
+            localStorage.setItem('user', response.data);
+
+            dispatch({ type: 'LOGIN', payload: response.data });
+            setIsLoading(false);
+        } catch (error) {
+            setErrors(getInputErrorMessages(error.response?.data))
+            setIsLoading(false);
+        }
+    }
+
+    return { login, isLoading, errors }
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,33 +5,66 @@ import {
     Container,
     Card,
     Row,
-    Col
+    Col,
+    Alert
 } from 'react-bootstrap';
 import Layout from '../pages/Layout';
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import { useLogin } from '../hooks/useLogin';
+import InputFeedback from '../components/InputFeedback';
 
 const Login = () => {
+    const { login, isLoading, errors } = useLogin();
+    
+    const formData = {
+        username: '',
+        password: ''
+    }
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+
+        formik.setFieldValue(name, value);
+    }
+
+    const formik = useFormik({
+        initialValues: formData,
+        onSubmit: async () => {
+            await login(formik.values);
+        }
+    });
+    
     return (
         <Layout>
             <Container>
                 <Row>
                     <Col md="5" sm="12" className="mx-auto">
                         <h3 className="text-center py-4">Welcome to Dreamhome</h3>
+                        {
+                            errors?.message
+                            ? <Alert variant="danger">
+                                {errors.message}
+                            </Alert>
+                            : null
+                        }
                         <Card className="mt-4 mb-4">
                             <Card.Body>
                                 <Card.Title className="text-center">Login to Your Account</Card.Title>
                                 <hr />
-                                <Form>
+                                <Form onSubmit={formik.handleSubmit}>
                                     <Form.Group className="mb-3" controlId="email">
-                                        <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" />
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control value={formik.values.username} onChange={handleInputChange} name="username" type="text" placeholder="Username" />
+                                        <InputFeedback message={errors?.field?.username}/>
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="password">
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" placeholder="Password" />
+                                        <Form.Control value={formik.values.password} onChange={handleInputChange} name="password" type="password" placeholder="Password" />
+                                        <InputFeedback message={errors?.field?.password}/>
                                     </Form.Group>
 
-                                    <Button variant="primary" type="submit" className="w-100 mt-2">
+                                    <Button disabled={isLoading} variant="primary" type="submit" className="w-100 mt-2">
                                         Login
                                     </Button>
                                     <div className="py-3 text-center">
